2d596782507e2ddd52697244cd2550a0814ab750
talebbechir123 <atbechir@etu.unistra.fr>
2026-06-14 18:05:59 +0200

    fix: remove unused useState import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./pages/Home";
